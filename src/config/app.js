2df896b2aa01feae4da656bd156d451c54903777
Esteven Javier Mc <EstevenJaviier@gmail.com>
2020-05-01 11:05:02 -0500

    group video

--- src/config/app.js
+++ src/config/app.js
@@ -4,17 +4,49 @@ import SocketIo from 'socket.io'
 module.exports = app => {
     const server = http.createServer(app);
     const io = SocketIo(server);
-
-    const { APP_PORT } = process.env;
+    const users = {};
+    const socketToRoom = {};
 
     io.on('connection', function (socket) {
         console.log(`a user connected: ${socket.id}`);
-        socket.on('disconnect', () => {
-            console.log(`a user disconnected: ${socket.id}`);
-        });
 
         socket.on('new-op', function (msg) {
             socket.broadcast.emit('new-remote-op', msg);
+        });
+
+        socket.on("join room", roomID => {
+            if (users[roomID]) {
+                const length = users[roomID].length;
+                if (length === 4) {
+                    socket.emit("room full");
+                    return;
+                }
+                users[roomID].push(socket.id);
+            } else {
+                users[roomID] = [socket.id];
+            }
+            socketToRoom[socket.id] = roomID;
+            const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+
+            socket.emit("all users", usersInThisRoom);
+        });
+
+        socket.on("sending signal", payload => {
+            io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+        });
+
+        socket.on("returning signal", payload => {
+            io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+        });
+
+        socket.on('disconnect', () => {
+            console.log(`a user disconnected: ${socket.id}`);
+            const roomID = socketToRoom[socket.id];
+            let room = users[roomID];
+            if (room) {
+                room = room.filter(id => id !== socket.id);
+                users[roomID] = room;
+            }
         });
     });
 
